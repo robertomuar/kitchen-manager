9b82bd6c4d755f21ebde13ff457bd2c09b5c72d2
Roberto Muñoz <roberto.muar@gmail.com>
2025-12-03 02:01:39 +0100

    Ensure auth requests carry CSRF tokens

--- resources/js/Components/DropdownLink.vue
+++ resources/js/Components/DropdownLink.vue
@@ -1,17 +1,42 @@
 <script setup>
 import { Link } from '@inertiajs/vue3';
 
-defineProps({
+const props = defineProps({
     href: {
         type: String,
         required: true,
+    },
+    method: {
+        type: String,
+        default: 'get',
+    },
+    as: {
+        type: String,
+        default: 'a',
+    },
+    data: {
+        type: Object,
+        default: () => ({}),
+    },
+    preserveScroll: {
+        type: Boolean,
+        default: false,
+    },
+    preserveState: {
+        type: Boolean,
+        default: false,
     },
 });
 </script>
 
 <template>
     <Link
-        :href="href"
+        :href="props.href"
+        :method="props.method"
+        :as="props.as"
+        :data="props.data"
+        :preserve-scroll="props.preserveScroll"
+        :preserve-state="props.preserveState"
         class="block w-full px-4 py-2 text-start text-sm leading-5 text-gray-700 transition duration-150 ease-in-out hover:bg-gray-100 focus:bg-gray-100 focus:outline-none"
     >
         <slot />
